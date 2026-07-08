57e9cad54c8213e6ac2a6a24265e3dc10c7e9fc1
matar <matar@bancolombia.com.co>
2026-07-07 21:11:56 -0500

    first changes

--- src/main/java/com/bolsadeideas/springboot/reactor/app/SpringBootReactorApplication.java
+++ src/main/java/com/bolsadeideas/springboot/reactor/app/SpringBootReactorApplication.java
@@ -1,13 +1,37 @@
 package com.bolsadeideas.springboot.reactor.app;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import reactor.core.publisher.Flux;
 
 @SpringBootApplication
-public class SpringBootReactorApplication {
+public class SpringBootReactorApplication implements CommandLineRunner {
+
+    private static final Logger log = LoggerFactory.getLogger(SpringBootReactorApplication.class);
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBootReactorApplication.class, args);
     }
 
+    /**
+     * Callback used to run the bean.
+     *
+     * @param args incoming main method arguments
+     * @throws Exception on error
+     */
+    @Override
+    public void run(String... args) throws Exception {
+        Flux<String> nombre = Flux.just("Andres", "", "Juanito")
+                .doOnNext(e -> {
+                    if (e.isEmpty()) {
+                        throw new RuntimeException("No se ha encontrado el nombre");
+                    }
+                    System.out.println(e);
+                });
+
+        nombre.subscribe(log::info, error -> log.error(error.getMessage()));
+    }
 }
